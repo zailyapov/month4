aa1e1a9178ecffa1b351b11958bd055b7759a4ba
zailyapov <115218244+zailyapov@users.noreply.github.com>
2023-03-22 13:54:30 +0600

    time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React from 'react';
 
-function App(){
-  return(
-    <div className="App">
-      <h1>Hello to the React</h1>
-    </div>
-  )
+class App extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            time: new Date()
+        }
+    }
+    upDateTime = () =>{
+        this.setState({
+            time: new Date()
+        })
+    }
+    intervalid = setInterval(this.upDateTime,1000)
+
+    render() {
+        return(
+            <div className='time'>
+                <h2>Время сейчас <br></br> {this.state.time.toLocaleTimeString()}</h2>
+            </div>
+        )
+    }
 }
-
-export default App
+export default App;
